a3594da993fb7c36a0c990b7a76ba5b4079f4c2d
usman <hafizusman3344mani@gmail.com>
2024-09-04 18:36:52 +0500

    Readme changed

--- lib/core/constants/app_secrets.dart
+++
@@ -1,6 +0,0 @@
-class AppSecrets {
-  static const String supaBaseUrl = 'https://btrgjdviqhznaksyxunp.supabase.co';
-  static const String newUrl = 'This is new Url';
-  static const String supaBaseApiKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0cmdqZHZpcWh6bmFrc3l4dW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyNTc0MTEsImV4cCI6MjA0MDgzMzQxMX0.wxPwSPt3s3YPdU6LFJX7BMYaHFYYLdRMOHWkc52qVj0';
-}
